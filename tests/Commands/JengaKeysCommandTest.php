b7c0920db07ebff3a70feb3f6de3d05ce1eaee25
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-02-11 01:53:36 +0300

    Update JengaKeysCommandTest.php

--- tests/Commands/JengaKeysCommandTest.php
+++ tests/Commands/JengaKeysCommandTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use phpseclib3\Crypt\Common\PrivateKey;
+use phpseclib3\Crypt\Common\PublicKey;
+
 beforeEach(function () {
     config()->set(key: 'jenga.keys_path', value: './tests/stubs');
 
@@ -15,9 +18,9 @@ beforeEach(function () {
     }
 });
 
-test(description: 'it generate and replace private and public keys if they do not exists', closure: function () {
-    expect(file_exists($this->privateKeyPath))->toBefalse()
-        ->and(file_exists($this->publiKeyPath))->toBefalse();
+test(description: 'it generates and create private and public keys when they do not exists', closure: function () {
+    expect(file_exists($this->privateKeyPath))->toBeFalse()
+        ->and(file_exists($this->publiKeyPath))->toBeFalse();
 
     $this->artisan(command: 'jenga:keys')
         ->assertSuccessful()
@@ -25,4 +28,27 @@ test(description: 'it generate and replace private and public keys if they do no
 
     expect(file_exists($this->privateKeyPath))->toBeTrue()
     ->and(file_exists($this->publiKeyPath))->toBeTrue();
+});
+
+test(description: 'it generates valid private key', closure: function () {
+    $this->artisan(command: 'jenga:keys')
+        ->assertSuccessful()
+        ->expectsOutput(output: trans(key: 'jenga::jenga.keys.generated'));
+
+    $privateKey = \phpseclib3\Crypt\RSA\PrivateKey::load(file_get_contents($this->privateKeyPath));
+
+    expect(value: $privateKey instanceof PrivateKey)->toBeTrue()
+        ->and(file_get_contents($this->privateKeyPath))->tobe($privateKey->toString('PKCS8'));
+});
+
+
+test(description: 'it generates valid public key', closure: function () {
+    $this->artisan(command: 'jenga:keys')
+        ->assertSuccessful()
+        ->expectsOutput(output: trans(key: 'jenga::jenga.keys.generated'));
+
+    $publicKey = \phpseclib3\Crypt\RSA\PublicKey::load(file_get_contents($this->publiKeyPath));
+
+    expect(value: $publicKey instanceof PublicKey)->toBeTrue()
+        ->and(file_get_contents($this->publiKeyPath))->tobe($publicKey->toString('PKCS8'));
 });
