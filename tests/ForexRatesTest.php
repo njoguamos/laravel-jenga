3803dcd7c38f39836e3c1395b6428327452fd3c1
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-02-13 20:29:35 +0300

    Create ForexRatesTest.php

---
+++ tests/ForexRatesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use NjoguAmos\Jenga\ForexRates;
+use NjoguAmos\Jenga\Models\JengaToken;
+
+test(description: 'it can get forex rates successfully', closure: function () {
+    $token = JengaToken::factory()->create();
+
+    $url = config(key: 'jenga.host') . '/v3-apis//transaction-api/v3.0/foreignExchangeRates';
+
+    $response = [
+        "status"  => true,
+        "code"    => 0,
+        "message" => "success",
+        "data"    => [
+            "convertedAmount" => 127749.2,
+            "rate"            => 122.6,
+            "fromAmount"      => 1042,
+            "rateCode"        => "TTB"
+        ]
+    ];
+
+    Http::fake([
+        $url => Http::response($response, 200),
+    ]);
+
+    Http::preventStrayRequests();
+
+    $rates = (new ForexRates())
+        ->convert(
+            amount: 1042,
+            currencyCode: "USD",
+            toCurrency: "KES",
+            accountNumber: '1450160649886',
+            countryCode: 'KE'
+        );
+
+    expect($rates)->toBe(json_encode($response));
+
+    Http::assertSent(function (Request $request) use ($url, $token) {
+        return $request->hasHeader('Authorization', "Bearer $token->access_token") &&
+            $request->url() == $url &&
+            $request['amount'] == 1042 &&
+            $request['currencyCode'] == "USD" &&
+            $request['toCurrency'] == "KES" &&
+            $request['accountNumber'] == "1450160649886" &&
+            $request['countryCode'] == "KE";
+    });
+});
