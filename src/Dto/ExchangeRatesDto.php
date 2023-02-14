b146b52393c7101dccdfb04d87a0366a906b0f22
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-02-14 23:01:16 +0300

    Create ExchangeRatesDto.php

---
+++ src/Dto/ExchangeRatesDto.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NjoguAmos\Jenga\Dto;
+
+class ExchangeRatesDto
+{
+    public function __construct(
+        /** The amount you wish to convert */
+        public int $amount,
+
+        /** The currency code of the currency (in ISO 4217 format) that is being converted from */
+        public string $currencyCode,
+
+        /** The currency code (in ISO 4217 format) of the currency being converted to */
+        public string $toCurrency,
+
+        /** Bank account registered in your JengaHQ. Default to test bank account. */
+        public ?string $accountNumber = null,
+
+        /** the country for which rates are being requested. Valid values are KE, TZ, UG, RW.*/
+        public string $countryCode = 'KE',
+    ) {
+    }
+}
